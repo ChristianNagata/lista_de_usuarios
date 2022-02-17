11af585f23f1d3590e8a625ffb7b74e8f6b8b194
Christian <christian_nagata@hotmail.com>
2022-02-17 15:09:33 -0300

    dados de usuários movidos para o arquivo dummy_users

--- lib/views/user_list.dart
+++ lib/views/user_list.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/components/user_tile.dart';
 import 'package:flutter_crud/data/dummy_users.dart';
-import '../models/user.dart';
 
 class UserList extends StatelessWidget {
   const UserList({Key? key}) : super(key: key);
@@ -9,31 +8,7 @@ class UserList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final Map<String, User> users = {
-      '1': User(
-        id: '1',
-        name: 'Maria',
-        email: 'maria@gmail.com',
-        avatarUrl:
-        'https://cdn.dribbble.com/users/2531150/screenshots/7381472/media/be977f32a3f5abf4898f2c343ab7bfe0.jpg?compress=1&resize=1200x900&vertical=top',
-      ),
-      '2': User(
-        id: '2',
-        name: 'Flavia',
-        email: 'flavia@gmail.com',
-        avatarUrl:
-        'https://cdn.dribbble.com/users/2531150/screenshots/7381483/media/498a13f429ddc746e6c072da892116ec.jpg?compress=1&resize=1200x900&vertical=top',
-      ),
-      '3': User(
-        id: '3',
-        name: 'Joao',
-        email: 'joao@gmail.com',
-        avatarUrl:
-        'https://cdn.dribbble.com/users/1355613/screenshots/15252730/media/28f348daf9654c440f5dcf398d8e097a.jpg?compress=1&resize=1200x900&vertical=top',
-      ),
-    };
-
-
+    final users = DUMMY_USERS;
 
     return Scaffold(
       appBar: AppBar(
